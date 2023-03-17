68830cb236e10a7e17acf071be5a88c49cd108f6
kithya <kithya.um@z1fsc.com>
2023-03-17 20:30:59 +0700

    update

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import mobile_scanner_pro
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+}
